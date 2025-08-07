8d19956f526bebc0e67f88e49f72a49d55747353
12332434565467 <18123825782@163.com>
2025-08-07 19:19:27 +0800

    Update IntegerDivision.asm

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -2,7 +2,7 @@
 D=M              // D = M[R1], i.e., the value stored at R1
 @DIVIDE_BY_ZERO  // Define label DIVIDE_BY_ZERO for division by zero handling
 D;JEQ            // Jump to DIVIDE_BY_ZERO if D == 0 (i.e., division by zero)
-
+ 
 @R0
 D=M
 @R5
@@ -21,7 +21,7 @@ D;JGE
 @ABS_R0
 M=-D
 (SKIP_R0_ABS)
-
+ 
 @R1
 D=M
 @R6
@@ -40,7 +40,7 @@ D;JGE
 @ABS_R1
 M=-D
 (SKIP_R1_ABS)
-
+ 
 @ABS_R0
 D=M
 @32768
@@ -49,7 +49,7 @@ D=D-A
 D;JEQ
 @START_DIVISION
 0;JMP
-
+ 
 (CHECK_OVERFLOW)
 @ABS_R1
 D=M
@@ -57,7 +57,8 @@ D=M
 D=D-A
 @OVERFLOW_ERROR
 D;JEQ
-
+@START_DIVISION  
+ 
 (START_DIVISION)
 @ABS_R0
 D=M
@@ -65,7 +66,7 @@ D=M
 M=D
 @R2
 M=0
-
+ 
 (LOOP)
 @ABS_R1
 D=M
@@ -81,8 +82,7 @@ M=M-D
 M=M+1
 @LOOP
 0;JMP
-
-
+ 
 (END_LOOP)
 @R5
 D=M
@@ -92,11 +92,11 @@ D=D-M
 D;JNE
 @SET_REMAINDER_SIGN
 0;JMP
-
+ 
 (SET_NEGATIVE_QUOTIENT)
 @R2
 M=-M
-
+ 
 (SET_REMAINDER_SIGN)
 @R5
 D=M
@@ -104,13 +104,13 @@ D=M
 D;JEQ
 @R3
 M=-M
-
+ 
 (SKIP_REMAINDER_NEG)
 @R4
 M=0
 @END
 0;JMP
-
+ 
 (DIVIDE_BY_ZERO)
 @R2
 M=0
@@ -120,7 +120,7 @@ M=0
 M=1
 @END
 0;JMP
-
+ 
 (OVERFLOW_ERROR)
 @R2
 M=0
@@ -130,7 +130,67 @@ M=0
 M=1
 @END
 0;JMP
-
+ 
 (END)
 @END
 0;JMP
+ 
+// SumArrayEntries.asm
+// Computes the sum of all entries in an array
+// R0: memory address of first array element
+// R1: number of elements in array
+// R2: final result (sum of array elements)
+// R3-R15: can be used as temporary variables
+ 
+// Check if array length is positive
+@R1
+D=M              // D = array length
+@ZERO_RESULT     // If length <= 0, set result to 0
+D;JLE
+ 
+// Initialize variables
+@R2
+M=0              // R2 = sum = 0
+@R0  
+D=M              // D = base address
+@R3
+M=D              // R3 = current pointer = base address
+@R1
+D=M              // D = array length
+@R4
+M=D              // R4 = counter = array length
+ 
+// Main loop: iterate through array elements
+(LOOP)
+@R4
+D=M              // D = counter
+@END_LOOP
+D;JEQ            // If counter == 0, exit loop
+ 
+@R3
+A=M              // A = current pointer address
+D=M              // D = M[current pointer] = current array element
+@R2
+M=M+D            // sum += current element
+ 
+@R3
+M=M+1            // pointer++, move to next element
+@R4
+M=M-1            // counter--, decrement counter
+@LOOP
+0;JMP            // Continue loop
+ 
+(END_LOOP)
+@END
+0;JMP
+ 
+(ZERO_RESULT)
+@R2
+M=0              // Set result to 0
+@END
+0;JMP
+ 
+(END)
+@END
+0;JMP
+ 
